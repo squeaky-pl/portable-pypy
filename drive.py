bf52837569e89db1e3e9a9ce8acc24d37c523d1f
Squeaky <squeaky_pl@gmx.com>
2014-10-24 00:52:18 +0200

    bump versions

--- drive.py
+++ drive.py
@@ -39,7 +39,7 @@ def urlcopy(src, dest=None, use_cache=True):
         dest = join(dest, basename(src))
 
     if urlparse(src).scheme:
-        check_call(['wget', '-O', dest, src])
+        check_call(['wget', '--no-check-certificate', '-O', dest, src])
 
         if use_cache and not exists(cached):
             copy2(dest, cache)
@@ -76,7 +76,7 @@ def unpack(src, dest, strip=0, excludes=None, okcode=None, use_cache=True):
 
 devtools, pypy = """
 https://bitbucket.org/squeaky/centos-devtools/downloads/gcc-4.8.2-binutils-2.23.2-{arch}.tar.bz2
-https://bitbucket.org/squeaky/portable-pypy/downloads/pypy-2.3.1-linux_{arch}-portable.tar.bz2
+https://bitbucket.org/squeaky/portable-pypy/downloads/pypy-2.4-linux_{arch}-portable.tar.bz2
 """.strip().split()
 
 
@@ -153,9 +153,9 @@ def runinroot(root, cmd, cwd=None, okcode=None, call=check_call):
 
 deps = """
 http://releases.nixos.org/patchelf/patchelf-0.8/patchelf-0.8.tar.gz
-http://sqlite.org/2014/sqlite-autoconf-3080600.tar.gz
+http://sqlite.org/2014/sqlite-autoconf-3080700.tar.gz
 http://www.mirrorservice.org/sites/sourceware.org/pub/libffi/libffi-3.1.tar.gz
-http://www.openssl.org/source/openssl-1.0.1i.tar.gz
+http://www.openssl.org/source/openssl-1.0.1j.tar.gz
 http://downloads.sourceforge.net/project/expat/expat/2.1.0/expat-2.1.0.tar.gz
 http://ftp.gnu.org/gnu/gdbm/gdbm-1.11.tar.gz
 http://prdownloads.sourceforge.net/tcl/tcl8.6.2-src.tar.gz
