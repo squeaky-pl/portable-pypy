140c776d35985b447ef890b8c5e2dd0ad9428a45
Squeaky <squeaky_pl@gmx.com>
2015-01-03 02:19:59 +0100

    32 bit works, but Makefile needs editing

--- runopt.sh
+++ runopt.sh
@@ -6,6 +6,8 @@ cat <<EOF
 
 -v `pwd`/src:/src
 -v `pwd`/build_deps:/src/build_deps
+-v `pwd`/build:/src/build
+-v `pwd`/platform_linux.patch:/src/platform_linux.patch
 -v `pwd`/prefix:/opt/prefix
 
 -e ABI=$ABI
